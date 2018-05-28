c285e877d1e04c9e965dd27f0126fd67e5e29d92
aaron.chen <aaron.chen@aispeech.com>
2018-05-28 18:47:06 +0800

    增加组织级的报表

--- src/app/components/chart/chart-design/src/design-progress/design-progress.component.ts
+++ src/app/components/chart/chart-design/src/design-progress/design-progress.component.ts
@@ -1,9 +1,9 @@
-import {Component, Input, OnInit} from "@angular/core";
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'design-progress',
   templateUrl: './design-progress.html',
-  styleUrls: ['./styles.scss']
+  styleUrls: ['./styles.scss'],
 })
 export class DesignProgressComponent implements OnInit {
   @Input() showTitle: boolean = false;
@@ -31,54 +31,54 @@ export class DesignProgressComponent implements OnInit {
         top: 'top',
         left: 'center',
         textStyle: {
-          fontSize: '15'
-        }
+          fontSize: '15',
+        },
       },
       tooltip: {
         trigger: 'item',
-        formatter: '{a} <br/>{b} : {c}'
+        formatter: '{a} <br/>{b} : {c}',
       },
       legend: {
         right: '0%',
         width: '15%',
-        data: ['新增用例数', '合计用例数']
+        data: ['新增用例数', '合计用例数'],
       },
       grid: {
         top: '15%',
-        right: '15%',
+        right: '1%',
         bottom: '5%',
         left: '1%',
         containLabel: true,
       },
-      color: ['#c23531','#2f4554'],
+      color: ['#c23531', '#2f4554'],
       xAxis: {
         type: 'category',
         name: '',
-        splitLine: {show: false},
+        splitLine: { show: false },
         data: this._data.xList,
-        axisLabel :{
+        axisLabel : {
           interval: 0,
           rotate: 45,
-          margin:20
+          margin: 20,
         },
       },
 
       yAxis: [
         {
           name: '新增（个）',
-          type: 'value'
+          type: 'value',
         },
         {
           name: '累计（个）',
-          type: 'value'
-        }
+          type: 'value',
+        },
       ],
       series: [
         {
-          name:'新增数量',
-          type:'bar',
+          name: '新增数量',
+          type: 'bar',
           yAxisIndex: 0,
-          data: this._data.numbList
+          data: this._data.numbList,
         },
         {
           name: '累计数量',
@@ -86,7 +86,7 @@ export class DesignProgressComponent implements OnInit {
           yAxisIndex: 1,
           data: this._data.totalList,
         },
-      ]
+      ],
     };
   }
 
