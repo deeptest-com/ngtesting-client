961a3c63df31275cb0250760c74b44007902522a
Aaron Chen <462826@qq.com>
2018-10-04 16:14:02 +0800

    问题查询

--- src/app/components/tql-condition/src/tql-condition.component.ts
+++ src/app/components/tql-condition/src/tql-condition.component.ts
@@ -1,7 +1,7 @@
 import * as _ from 'lodash';
 
-import { Component, Input, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, Input, OnInit, AfterViewInit, Output, EventEmitter, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
 
 import { CONSTANT } from '../../../utils/constant';
@@ -13,25 +13,25 @@ import { TqlConditionService } from './tql-condition.service';
   templateUrl: './tql-condition.html',
   styleUrls: ['./styles.scss'],
 })
-export class TqlConditionComponent implements OnInit {
+export class TqlConditionComponent implements OnInit, AfterViewInit {
   form: FormGroup;
 
+  @Input() filter: boolean = true;
   @Input() title: string;
   @Input() items: any[];
   @Output() selected = new EventEmitter<any>();
 
   constructor(private fb: FormBuilder, private tqlConditionService: TqlConditionService) {
-    this.form = this.fb.group(
-      {
-        'selectedItems': ['', []],
-      }, {},
-    );
-
-    this.form.valueChanges.debounceTime(CONSTANT.DebounceTime).subscribe(values => this.change(values));
+    this.form = this.fb.group({});
   }
 
   ngOnInit(): any {
-
+    _.forEach(this.items, (item: any, index: number) => {
+      this.form.addControl('menu-item-' + item.id, new FormControl('', []));
+    });
+  }
+  ngAfterViewInit() {
+    this.form.valueChanges.debounceTime(CONSTANT.DebounceTime).subscribe(values => this.change(values));
   }
 
   change(values: any) {
@@ -47,6 +47,13 @@ export class TqlConditionComponent implements OnInit {
     });
 
     this.selected.emit({ type: 'project', data: selectedItems });
+  }
+
+  clickMenu($event): any {
+    $event.stopPropagation();
+  }
+  selectItem(item): any {
+    item.selected = !item.selected;
   }
 
 }
