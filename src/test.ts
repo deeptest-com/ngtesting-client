4311e24658a7c5feb3f867dc65b27b854665a3c7
Aaron Chen <462826@qq.com>
2018-12-08 23:54:40 +0800

    问题编辑页面

--- src/test.ts
+++ src/test.ts
@@ -28,5 +28,6 @@ getTestBed().initTestEnvironment(
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
+
 // Finally, start Karma to run the tests.
 __karma__.start();
